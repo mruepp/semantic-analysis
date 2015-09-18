9f35f55af38437361a7f6876a475b50d165a3bb4
Michael Ruepp <michael@ruepp.at>
2015-09-18 09:30:27 +0200

    Change URLs
    Former-commit-id: 2943ee4848502dfc5b15324877f2fe9114218574

--- src/main/java/com/pianobakery/complsa/MainGui.java
+++ src/main/java/com/pianobakery/complsa/MainGui.java
@@ -6,6 +6,7 @@ import com.license4j.License;
 import com.license4j.ValidationStatus;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.filefilter.*;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
@@ -33,11 +34,14 @@ import java.io.*;
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
 import java.net.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.DecimalFormat;
 import java.util.*;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 
 /**
@@ -141,6 +145,8 @@ public class MainGui {
     private static String emptyTable = "No Search Result...";
     private static String helpURL = "http://www.oyonoko.com/semanticsearch/help";
     private static String updateURL = "http://www.oyonoko.com/semanticsearch/download";
+
+    private static final int BUFFER_SIZE = 4096;
 
 
     private static JMenuItem newAction = new JMenuItem("New Folder");
@@ -1112,6 +1118,23 @@ public class MainGui {
 
     public void downloadModelMethod() {
         logger.debug("Download Models");
+        /*File theFolder = new File(wDir + File.separator + trainModelFolder);
+
+        InputStream trainModels = getClass().getResourceAsStream("TrainModels.zip");
+        File trainModelsFile = new File(trainModels.getPath());
+
+        try {
+            unzip(trainModelsFile.toString(), theFolder.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            addExistingSentModelsToMap();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
+
 
         try {
             if (testURL(modelUrl)) {
@@ -1122,6 +1145,8 @@ public class MainGui {
             JOptionPane.showMessageDialog(null, "Connection Timeout - check your Internet connection");
         }
     }
+
+
 
     public void addTopicCorpusMethod() {
 
@@ -2500,6 +2525,41 @@ public class MainGui {
             }
         }
     }
+
+    private void extractFile(ZipInputStream zipIn, String filePath) throws IOException {
+        BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(filePath));
+        byte[] bytesIn = new byte[BUFFER_SIZE];
+        int read = 0;
+        while ((read = zipIn.read(bytesIn)) != -1) {
+            bos.write(bytesIn, 0, read);
+        }
+        bos.close();
+    }
+
+    public void unzip(String zipFilePath, String destDirectory) throws IOException {
+        File destDir = new File(destDirectory);
+        if (!destDir.exists()) {
+            destDir.mkdir();
+        }
+        ZipInputStream zipIn = new ZipInputStream(new FileInputStream(zipFilePath));
+        ZipEntry entry = zipIn.getNextEntry();
+        // iterates over entries in the zip file
+        while (entry != null) {
+            String filePath = destDirectory + File.separator + entry.getName();
+            if (!entry.isDirectory()) {
+                // if the entry is a file, extracts it
+                extractFile(zipIn, filePath);
+            } else {
+                // if the entry is a directory, make the directory
+                File dir = new File(filePath);
+                dir.mkdir();
+            }
+            zipIn.closeEntry();
+            entry = zipIn.getNextEntry();
+        }
+        zipIn.close();
+    }
+
 
     /*
     public void addRemoveItemToTopicBox(File aFile, Boolean add, boolean isTopicCorp) throws IOException {
